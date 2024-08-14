0eab598af9edb2e39f3cb739b7f5f6895705d8ca
Mary Mhar Perez <marymharperez@gmail.com>
2024-08-15 00:32:06 +0800

    Midterm Project

---
+++ MidtermProj/MidtermProj/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace MidtermProj
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Calculate_Clicked(object sender, EventArgs e)
+        {
+            double principalAmount = Convert.ToDouble(txtPrincipalAmount.Text);
+            int terms = Convert.ToInt32(pickerTerms.SelectedItem);
+            double serviceFee = principalAmount * 0.015 >= 1500 ? 1500 : principalAmount * 0.015;
+            double fillingFee = principalAmount * 0.015 >= 1500 ? 1500 : principalAmount * 0.015;
+            double insuranceFee = principalAmount * 0.015 * terms / 12;
+            double savingRetention = principalAmount * 0.01;
+            double monthlyAmortization = (principalAmount * 0.015) * Math.Pow((1 + 0.015), terms) / Math.Pow((1 + 0.015) -1, terms);
+            double balance = Convert.ToDouble(txtBalance.Text);
+            double netLoanProceeds = principalAmount - (balance + serviceFee + fillingFee + insuranceFee + savingRetention);
+
+            Navigation.PushAsync(new ResultPage(
+                txtName.Text, txtDate.Text, pickerLoanType.SelectedItem.ToString(), principalAmount, terms,
+                serviceFee, fillingFee, insuranceFee, savingRetention, monthlyAmortization, balance, netLoanProceeds));
+        }
+    }
+    }
---
+++ MidtermProj/MidtermProj/Page1.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MidtermProj
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ResultPage : ContentPage
+    {
+        public ResultPage(string name, string date, string loanType, double principalAmount, int terms,
+            double serviceFee, double fillingFee, double insuranceFee, double savingRetention,
+            double monthlyAmortization, double balance, double netLoanProceeds)
+        {
+            InitializeComponent();
+
+            lblName.Text = "" + name;
+            lblDate.Text = "" + date;
+            lblLoanType.Text = "" + loanType;
+            lblPrincipalAmount.Text = "" + principalAmount.ToString("N2");
+            lblTerms.Text = "" + terms;
+            lblServiceFee.Text = "" + serviceFee.ToString("N2");
+            lblFillingFee.Text = "" + fillingFee.ToString("N2");
+            lblInsuranceFee.Text = "" + insuranceFee.ToString("N2");
+            lblSavingRetention.Text = "" + savingRetention.ToString("N2");
+            lblMonthlyAmortization.Text = "" + monthlyAmortization.ToString("N2");
+            lblBalance.Text = "" + balance.ToString("N2");
+            lblNetLoanProceeds.Text = "" + netLoanProceeds.ToString("N2");
+        }
+    }
+}
